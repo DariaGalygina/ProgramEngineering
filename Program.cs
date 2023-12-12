43921ce6b59e8f3bfd1808fdc2f82830f7f05c8c
DariaGalygina <dasha.galygina@mail.ru>
2023-12-12 15:08:51 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    // Функция для проверки числа на простоту
+    static bool IsPrime(int number)
+    {
+        if (number <= 1)
+        {
+            return false;
+        }
+        for (int i = 2; i <= Math.Sqrt(number); i++)
+        {
+            if (number % i == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        if (IsPrime(number))
+        {
+            Console.WriteLine($"{number} - простое число");
+        }
+        else
+        {
+            Console.WriteLine($"{number} - не является простым числом");
+        }
+    }
+}
